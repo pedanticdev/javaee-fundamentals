1e14712be755515b5a27957403c1090b4e1a6885
Seeraj <sinaisix@gmail.com>
2017-10-14 09:09:20 +0000

    Added async event firing

--- src/main/java/com/pedantic/cdi/course/beans/EventBean.java
+++ src/main/java/com/pedantic/cdi/course/beans/EventBean.java
@@ -9,9 +9,12 @@ import com.pedantic.cdi.course.events.User;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.CompletionStage;
 
 @Web
 public class EventBean {
+
     @Inject
     private EventData eventData;
     @Inject
@@ -27,16 +30,22 @@ public class EventBean {
 
     public void login() {
         //Do credentials checking to login in user then fire login event
-
         //someSecurityManager.loginUser(user.getEmail, user.getPassword)
 
-        eventDataEvent.fire(new EventData(user.getEmail(), LocalDateTime.now()));
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+
+//        eventDataEvent.fire(new EventData(user.getEmail(), LocalDateTime.now()));
+        CompletionStage<EventData> fireAsync = eventDataEvent.fireAsync(new EventData(user.getEmail(), LocalDateTime.now()));
+
+        long secs = ChronoUnit.SECONDS.between(now, LocalDateTime.now());
+
+        System.out.println("It took us this number of seconds to login " + secs);
+
         //Qualified Observer
         conditionalEvent.fire(new EventData(user.getEmail(), LocalDateTime.now()));
-       
 
     }
-
 
     public User getUser() {
         return user;
--- src/main/java/com/pedantic/cdi/course/events/EventObserver.java
+++ src/main/java/com/pedantic/cdi/course/events/EventObserver.java
@@ -5,6 +5,7 @@ import com.pedantic.cdi.course.annotations.PopularStand;
 
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.event.Observes;
+import javax.enterprise.event.ObservesAsync;
 import javax.enterprise.event.Reception;
 import javax.enterprise.event.TransactionPhase;
 import javax.inject.Inject;
@@ -13,19 +14,43 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RequestScoped
-public class EventObserver implements Serializable{
-
+public class EventObserver implements Serializable {
+    
     @Inject
     private Logger logger;
+    
+//    void userLoggedIn(@Observes @PopularStand EventData eventData) {
+//        //Persist in databse, send to another application outside your app
+//        //Essentially you can do whatever you want with the event data here.
+//        //We will just log it
+//        logger.log(Level.INFO, "User {0} logged in at {1}",
+//                new Object[]{eventData.getEmail(), eventData.getLoginTime()});
+//        try {
+//            Thread.sleep(6000);
+//        } catch (InterruptedException e) {
+//            logger.log(Level.SEVERE, null, e);
+//        }
+//    }
 
-    void userLoggedIn(@Observes @PopularStand EventData eventData) {
+
+    void asyncObserver(@ObservesAsync @PopularStand EventData eventData) {
         //Persist in databse, send to another application outside your app
         //Essentially you can do whatever you want with the event data here.
         //We will just log it
         logger.log(Level.INFO, "User {0} logged in at {1}",
                 new Object[]{eventData.getEmail(), eventData.getLoginTime()});
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            logger.log(Level.SEVERE, null, e);
+        }
     }
-
+    
+    
+    
+    
+    
+    
     void conditionalObserver(@Observes(notifyObserver = Reception.IF_EXISTS,
             during = TransactionPhase.AFTER_COMPLETION) @Admin EventData eventData) {
         logger.log(Level.INFO, "The CEO {0} logged in at {1}",
